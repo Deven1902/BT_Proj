449f009c76d6b8bd26b6ac9a13754698ce27f490
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-10-22 09:54:54 +0530

    Update parking.sol

--- ParkingLotApp/parking.sol
+++ ParkingLotApp/parking.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0; 
+pragma solidity ^0.8.0;
 
 contract ParkingLot {
     struct ParkingSpot {
@@ -25,4 +25,47 @@ contract ParkingLot {
         _;
     }
 
-    
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function addParkingSpot() external onlyOwner {
+        parkingSpotCount++;
+        parkingSpots[parkingSpotCount] = ParkingSpot(parkingSpotCount, false, address(0), 0);
+        emit ParkingSpotAdded (parkingSpotCount);
+
+    }
+
+     function checkIn(uint256 _spotId) external payable { 
+        ParkingSpot storage spot = parkingSpots[_spotId]; 
+        require(!spot.isOccupied, "Spot is already occupied"); 
+        require(msg.value == pricePerHour, "Incorrect payment");
+        
+        spot.isOccupied = true; 
+        spot.occupant = msg.sender;
+        spot.checkInTime = block.timestamp;
+
+        emit CheckedIn(_spotId, msg.sender);
+    } 
+
+    function checkOut(uint256 _spotId) external {
+        ParkingSpot storage spot = parkingSpots[_spotId];
+        require(spot.isOccupied && spot.occupant == msg.sender, "Not the occupant of this spot");
+        
+        uint256 duration = (block.timestamp - spot.checkInTime) / 1 hours;
+        uint256 totalCost = duration * pricePerHour;
+        
+        require(address(this).balance >= totalCost, "Contract balance insufficient");
+        
+        spot.isOccupied = false; spot.occupant = address(0); spot.checkInTime = 0;
+        payable(msg.sender).transfer(totalCost);
+        
+        emit CheckedOut(_spotId, msg.sender, duration, totalCost);
+    } 
+
+    function withdrawFunds() external onlyOwner{
+        uint256 balance = address(this).balance;
+        payable(owner).transfer(balance);
+    }
+
+}
